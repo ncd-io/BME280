e22bdbb7e64de6d7ecd8ab8c57622722d920fb2f
Trey Felton <trey@tomadoh.com>
2019-04-03 11:01:31 -0500

    Correct data types for compensation formulas

--- Particle/BME280.cpp
+++ Particle/BME280.cpp
@@ -20,11 +20,9 @@ void BME280::init(){
 void BME280::loop(){
     if(loop_delay == 0){
         loop_delay = readRate();
-        
-        Particle.publish("loop_delay", String(loop_delay));
     }
     int now = millis();
-    if(now-last_checked > loop_delay){
+    if(now-last_checked > 1000){
         last_checked = now;
         takeReadings();
     }
@@ -33,7 +31,7 @@ void BME280::loop(){
 void BME280::getParams(){
     
     int ptdata[25];
-    readBytes(0x88, ptdata, 25);
+    readBytes(0x88, ptdata, 26);
     temp_comp[0] = ptdata[0] + (ptdata[1] << 8);
     temp_comp[1] = ptdata[2] + (ptdata[3] << 8);
     temp_comp[2] = ptdata[4] + (ptdata[5] << 8);
@@ -48,17 +46,20 @@ void BME280::getParams(){
     press_comp[7] = ptdata[20] + (ptdata[21] << 8);
     press_comp[8] = ptdata[22] + (ptdata[23] << 8);
     
-    hum_comp[0] = ptdata[24];
+    hum_comp[0] = ptdata[25];
     
-    int humdata[8];
-    readBytes(0xE1, humdata, 8);
+    int humdata[7];
+    readBytes(0xE1, humdata, 7);
     hum_comp[1] = humdata[0] + (humdata[1] << 8);
     hum_comp[2] = humdata[2];
     hum_comp[3] = (humdata[3] << 4) + (humdata[4] & 0x0F);
-    hum_comp[4] = (humdata[5] >> 4) + (humdata[6] >> 4);
-    hum_comp[5] = humdata[7];
+    hum_comp[4] = (humdata[4] >> 4) + (humdata[5] << 4);
+    hum_comp[5] = humdata[6];
+    char HumComp[100];
+    sprintf(HumComp, "[%i,%i,%i,%i,%i,%i,%i]", hum_comp[0], hum_comp[1], hum_comp[2], hum_comp[3], hum_comp[4], hum_comp[5], hum_comp[6]);
+    Particle.publish("HumComp", HumComp);
 }
-
+// [75,352,0,344,50,30,0]
 void BME280::setAddress(int a0){
     address += a0;
 }
@@ -118,12 +119,16 @@ double BME280::takeReadings(){
     int vardata[8];
     readBytes(BME280_READ_PRESS_MSB, vardata, 8);
     
-    long press_var = (vardata[0] << 12) + (vardata[1] << 4) + (vardata[2] >> 4);
-    long temp_var = (vardata[3] << 12) + (vardata[4] << 4) + (vardata[5] >> 4);
+    int press_var = (vardata[0] << 12) + (vardata[1] << 4) + (vardata[2] >> 4);
+    int temp_var = (vardata[3] << 12) + (vardata[4] << 4) + (vardata[5] >> 4);
     int hum_var = (vardata[6] << 8) + vardata[7];
     
-    double tvar1 = (((double)temp_var)/16384 - ((double)temp_comp[0])/1024) * ((double)temp_comp[1]);
-    double tvar2 = ((((double)temp_var)/131072 - ((double)temp_comp[0])/8192) * (((double)temp_var)/131072 - ((double)temp_comp[0])/8192)) * ((double)temp_comp[2]);
+    unsigned short t0 = temp_comp[0];
+    short t1 = temp_comp[1];
+    short t2 = temp_comp[2];
+    
+    double tvar1 = (((double)temp_var)/16384 - ((double)t0)/1024) * ((double)t1);
+    double tvar2 = ((((double)temp_var)/131072 - ((double)t0)/8192) * (((double)temp_var)/131072 - ((double)t0)/8192)) * ((double)t2);
     
     double t_fine = (int)(tvar1+tvar2);
     
@@ -131,24 +136,46 @@ double BME280::takeReadings(){
     
     temperature = getTemp(scale);
     
+    unsigned short p0 = press_comp[0];
+    short p1 = press_comp[1];
+    short p2 = press_comp[2];
+    short p3 = press_comp[3];
+    short p4 = press_comp[4];
+    short p5 = press_comp[5];
+    short p6 = press_comp[6];
+    short p7 = press_comp[7];
+    short p8 = press_comp[8];
+    
     pressure = 0;
     double pvar1 = (t_fine/2) - 64000;
-    double pvar2 = pvar1 * pvar1 * ((double)press_comp[5]) / 32768;
-    pvar2 = pvar2 + pvar1 * ((double)press_comp[4]) * 2;
-    pvar2 = (pvar2/4) + (((double)press_comp[3]) * 65536);
-    pvar1 = (((double)press_comp[2]) * pvar1 * pvar1 / 524288 + ((double)press_comp[1]) * pvar1) / 524288;
-    pvar1 = (1 + pvar1 / 32768) * ((double)press_comp[0]);
+    double pvar2 = pvar1 * pvar1 * ((double)p5) / 32768;
+    pvar2 = pvar2 + pvar1 * ((double)p4) * 2;
+    pvar2 = (pvar2/4) + (((double)p3) * 65536);
+    pvar1 = (((double)p2) * pvar1 * pvar1 / 524288 + ((double)p1) * pvar1) / 524288;
+    pvar1 = (1 + pvar1 / 32768) * ((double)p0);
     if(pvar1>0){
         pressure = 1048576 - (double)press_var;
         pressure = (pressure - (pvar2 / 4096)) * 6250 / pvar1;
-        pvar1 = ((double)press_comp[8]) * pressure * pressure / 2147483648;
-        pvar2 = pressure * ((double)press_comp[7]) / 32768;
-        pressure = pressure + (pvar1 + pvar2 + ((double)press_comp[6])) / 16;
+        pvar1 = ((double)p8) * pressure * pressure / 2147483648;
+        pvar2 = pressure * ((double)p7) / 32768;
+        pressure = pressure + (pvar1 + pvar2 + ((double)p6)) / 16;
     }
     
     humidity = t_fine - 76800;
-    humidity = (hum_var - (((double)hum_comp[3]) * 64 + ((double)hum_comp[4]) / 16384 * humidity)) * (((double)hum_comp[1]) / 65536 * (1 + ((double)hum_comp[5]) / 67108864 * humidity * (1 + ((double)hum_comp[2]) / 67108864 * humidity)));
-    humidity = humidity * (1 - ((double)hum_comp[0]) * humidity / 524288);
+    
+    char h0 = hum_comp[0];
+    short h1 = hum_comp[1];
+    char h2 = hum_comp[2];
+    short h3 = hum_comp[3];
+    short h4 = hum_comp[4];
+    signed char h5 = hum_comp[5];
+    
+    humidity = (hum_var - (((double)h3) * 64 + ((double)h4) / 16384 * humidity)) * (((double)h1) / 65536 * (1 + ((double)h5) / 67108864 * humidity * (1 + ((double)h2) / 67108864 * humidity)));
+    
+    
+    
+    humidity = humidity * (1 - ((double)h0) * humidity / 524288);
+    
     
     if(humidity > 100) humidity = 100;
     else if(humidity < 0) humidity = 0;
